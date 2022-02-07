b8c81f9421ceeeafbe6359ce80326086b3125d34
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-02-07 16:22:29 -0400

    implementando modelo de aluno

---
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -0,0 +1,7 @@
+package br.com.alura.agenda.model;
+
+public class Aluno {
+    public Aluno(String nome, String telefone, String email) {
+
+    }
+}
--- app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -3,8 +3,13 @@ package br.com.alura.agenda.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.alura.agenda.R;
+import br.com.alura.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +17,20 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+                new Aluno(nome, telefone, email);
+            }
+        });
     }
 }
